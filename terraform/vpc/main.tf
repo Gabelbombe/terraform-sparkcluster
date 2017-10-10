dda5a2a8678e2d277db1b702f143b63ef0631461
ehime <jd.daniel@salmon.com>
2017-10-09 22:00:58 -0700

    adding vpc tf buildout

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,19 @@
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
+
+## Variables
+## NOTE: Upsert from vars file later
+variable "vpc_cidr" {}
+
+variable "subnet_cidr" {}
+
+## Outputs
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "subnet_id" {
+  value = "${aws_subnet.main.id}"
+}
+
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>#
+
